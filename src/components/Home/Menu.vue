850e496ce4dff449ab1888da6e62c357f1fca381
ThibautMilville <tmilville.pro@gmail.com>
2024-01-10 13:37:38 +0100

    Shop page

--- src/components/Home/Menu.vue
+++ src/components/Home/Menu.vue
@@ -1,11 +1,26 @@
 <style></style>
 
 <template>
-  <div class="flex items-end justify-center gap-x-[50px] h-full ">
-   
+  <div class="flex items-end justify-center gap-x-[50px] h-full">
+    <!-- Shop -->
+    <div class="absolute bottom-0 left-0 ml-6 z-10">
+      <NuxtLink to="/shop" class="flex justify-center mb-6 group">
+      <button
+        class="flex justify-center py-2 w-32 h-32 items-center border-white rounded-3xl bg-gradient-to-tl from-gray-900 via-gray-800 to-gray-900"
+      >
+        <button class="z-20 group">
+          <img
+            src="~/assets/img/coin.png"
+            class="w-20 mx-auto transition duration-700 opacity-60 group-hover:opacity-100 group-hover:scale-105"
+          />
+          <div class="text-2xl uppercase text-white mt-2">Shop</div>
+        </button>
+      </button>
+    </NuxtLink>
+    </div>
     <NuxtLink to="/inventory" class="flex justify-center mb-6 group">
       <button
-        class="flex justify-center py-2 w-32 h-32 items-center  border-white rounded-3xl  bg-gradient-to-tl from-gray-900 via-gray-800 to-gray-900"
+        class="flex justify-center py-2 w-32 h-32 items-center border-white rounded-3xl bg-gradient-to-tl from-gray-900 via-gray-800 to-gray-900"
       >
         <button class="z-20 group">
           <img
@@ -13,11 +28,13 @@
             class="w-20 mx-auto transition duration-700 opacity-60 group-hover:opacity-100 group-hover:scale-105"
           />
           <div class="text-2xl uppercase text-white mt-2">Bago</div>
-
         </button>
       </button>
     </NuxtLink>
-    <NuxtLink to="/map" class="group mb-6 bg-gradient-to-tl from-gray-900 via-yellow-900 to-yellow-900 rounded-full border-2 p-3">
+    <NuxtLink
+      to="/map"
+      class="group mb-6 bg-gradient-to-tl from-gray-900 via-yellow-900 to-yellow-900 rounded-full border-2 p-3"
+    >
       <button
         class="flex justify-center py-4 w-36 h-36 items-center border-2 border-white rounded-3xl bg-gradient-to-tl from-gray-300 via-gray-500 to-gray-400"
       >
@@ -32,7 +49,7 @@
     </NuxtLink>
     <NuxtLink to="/invocations" class="flex justify-center mb-6 group">
       <button
-        class="flex justify-center py-2 w-32 h-32 items-center  border-white rounded-3xl  bg-gradient-to-tl from-gray-900 via-gray-800 to-gray-900"
+        class="flex justify-center py-2 w-32 h-32 items-center border-white rounded-3xl bg-gradient-to-tl from-gray-900 via-gray-800 to-gray-900"
       >
         <button class="flex flex-col items-center gap-y-2 z-20">
           <img
@@ -40,12 +57,8 @@
             class="w-16 mx-auto transition duration-700 opacity-60 group-hover:opacity-100 group-hover:scale-105"
           />
           <div class="text-2xl uppercase text-white">Invocation</div>
-
         </button>
       </button>
     </NuxtLink>
-
-    
-    
   </div>
 </template>
---
+++ src/pages/shop/index.vue
@@ -0,0 +1,24 @@
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const userCards = ref([]);
+</script>
+
+<template>
+  <div>
+    <!-- Fixed background -->
+    <div
+      class="w-full h-screen bg-[url('~/assets/img/background_invocation.png')] bg-center bg-cover fixed top-0 -z-20"
+    ></div>
+
+    <!-- Page content -->
+    <div class="relative z-10 p-4">
+      <h2
+        class="text-4xl font-semibold text-center text-gray-800 py-6 mb-4 text-white"
+      >
+        SHOP
+      </h2>
+    </div>
+  </div>
+</template>
