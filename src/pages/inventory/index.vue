f0c3c4df1a9c52001334b7ed3a77dcf2c9fefcfc
ThibautMilville <tmilville.pro@gmail.com>
2024-01-11 14:29:11 +0100

    Inventory display

--- src/pages/inventory/index.vue
+++ src/pages/inventory/index.vue
@@ -1,6 +1,5 @@
-
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch, computed } from 'vue';
 import { useUserStore } from '~/store/user';
 
 const userCards = ref([]);
@@ -12,7 +11,7 @@ onMounted(() => {
   if (userEmail) {
     loadUserCardsByEmail(userEmail);
   } else {
-    console.log("User email is not defined.");
+    console.log('User email is not defined.');
   }
 });
 
@@ -25,9 +24,9 @@ watch(
   }
 );
 
-async function loadUserCardsByEmail(userEmail) {
+async function loadUserCardsByEmail(userEmail: string) {
   const { data, error } = await supabase
-    .from("user_cards")
+    .from('user_cards')
     .select(
       `
       *,
@@ -36,12 +35,31 @@ async function loadUserCardsByEmail(userEmail) {
       )
     `
     )
-    .eq("email_user", userEmail);
+    .eq('email_user', userEmail);
 
   if (error) {
-    console.log("Error", error);
+    console.log('Error', error);
   } else {
     userCards.value = data;
+  }
+}
+
+function filterUserCardsByRarity(rarity: string) {
+  return userCards.value.filter((card: { cards: any }) => card.cards.rarity === rarity) as { id: string }[];
+}
+
+function getRarityLabel(rarity: string) {
+  switch (rarity) {
+    case 'lr':
+      return 'Légendaire';
+    case 'ssr':
+      return 'Super Rare';
+    case 'sr':
+      return 'Rare';
+    case 'r':
+      return 'Commun';
+    default:
+      return '';
   }
 }
 </script>
@@ -50,20 +68,22 @@ async function loadUserCardsByEmail(userEmail) {
   <NuxtLayout name="custom">
     <div>
       <!-- Fixed background -->
-      <div
-        class="w-full h-screen bg-[url('~/assets/img/background_invocation.png')] bg-center bg-cover fixed top-0 -z-20"
-      ></div>
+      <div class="w-full h-screen bg-[url('~/assets/img/background_invocation.png')] bg-center bg-cover fixed top-0 -z-20"></div>
 
-      <!-- Page content -->
+      <!-- Content -->
       <div class="relative z-10 p-4">
-        <h2
-          class="text-4xl font-semibold text-center text-gray-800 py-6 mb-4 text-white"
-        >
-          Le contenu de l'inventaire:
-        </h2>
-        <ul class="grid grid-cols-1 mt-16 md:grid-cols-9 lg:grid-cols-6 gap-4 m-4">
-          <InventoryCard v-for="card in userCards" :key="card.id" :card="card" />
-        </ul>
+        <h2 class="text-4xl font-semibold text-center text-gray-800 py-6 mb-4 text-white">Le contenu de l'inventaire:</h2>
+
+        <template v-for="rarity in ['lr', 'ssr', 'sr', 'r']">
+          <div v-if="filterUserCardsByRarity(rarity).length > 0">
+            <h2 class="text-4xl font-semibold text-left text-gray-800 pt-6 text-white ml-4">
+              {{ getRarityLabel(rarity) }}
+            </h2>
+            <ul class="grid grid-cols-1 mt-10 md:grid-cols-9 lg:grid-cols-6 gap-4 m-4">
+              <InventoryCard v-for="card in filterUserCardsByRarity(rarity)" :key="card.id" :card="card" />
+            </ul>
+          </div>
+        </template>
       </div>
     </div>
   </NuxtLayout>
